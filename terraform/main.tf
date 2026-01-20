8fcd01d0bab4847b958def1bc110197d30f87f03
Harsh <cloudopsengineerharsh@gmail.com>
2026-01-21 01:16:21 +0530

    Modified eks module

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,16 +28,17 @@ module "vpc" {
 
 
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
+  source  = "terraform-aws-modules/eks/aws"
   version = "20.8.5"
-  name    = "demo-eks"
-  kubernetes_version = "1.29"
+
+  cluster_name    = "demo-eks"
+  cluster_version = "1.29"
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
-  endpoint_public_access  = true
-  endpoint_private_access = true
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = true
 
   enable_irsa = true
 
@@ -55,26 +56,6 @@ module "eks" {
         worker = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
         cni    = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
         ecr    = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-      }
-
-      additional_security_group_rules = {
-        ingress_cluster_to_node_kubelet = {
-          description                   = "Cluster API to kubelet"
-          protocol                      = "tcp"
-          from_port                     = 10250
-          to_port                       = 10250
-          type                          = "ingress"
-          source_cluster_security_group = true
-        }
-
-        ingress_self_all = {
-          description = "Node to node communication"
-          protocol    = "-1"
-          from_port   = 0
-          to_port     = 0
-          type        = "ingress"
-          self        = true
-        }
       }
     }
   }
