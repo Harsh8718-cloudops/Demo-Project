f67a9cdc8193d2b4e2b02a4618d934bf6ace74fa
Harsh <cloudopsengineerharsh@gmail.com>
2026-01-21 00:58:41 +0530

    Modified main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,32 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  name    = "demo-vpc"
+  cidr    = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+}
+
+
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   name    = "demo-eks"
